06f851168378451f0296ffa7b272c45fa5e761ed
Polash Mahmud <polashmahmud@gmail.com>
2023-07-29 16:21:30 +0600

    add sidebar

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -16,10 +16,6 @@
     @endif
 
     <x-slot name="side">
-        <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-            <div class="p-6 text-gray-900 dark:text-gray-100">
-                Side
-            </div>
-        </div>
+       <x-side-bar></x-side-bar>
     </x-slot>
 </x-blog-layout>
